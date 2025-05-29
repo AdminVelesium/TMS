0a3d4ebdd2700201765da371160dd4df8cddce09
Meghs0205 <162950793+Meghs0205@users.noreply.github.com>
2025-05-29 15:04:19 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Cleaning old build..."
+sudo rm -rf /var/www/html/*
+
+echo "Copying new build..."
+sudo cp -r ~/app/* /var/www/html/
+
+echo "Restarting Nginx..."
+sudo systemctl restart nginx
+
+echo "🚀 Deployed successfully"
